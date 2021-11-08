29441ae935ef6488cf4f822c40163155795c7b62
King Josaphat Chewa <26507383+Kingjosh007@users.noreply.github.com>
2021-11-08 23:33:04 +0100

    Add TodoContainer.js component and fix ESLint errors

---
+++ src/components/TodoContainer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// eslint-disable-next-line react/prefer-stateless-function
+class TodoContainer extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello from Create React App</h1>
+        <p>I am in a React Component!</p>
+      </div>
+    );
+  }
+}
+export default TodoContainer;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import './index.css';
+import TodoContainer from './components/TodoContainer';
 
-const element = <h1>Hello from Create React App</h1>;
-
-ReactDOM.render(element, document.getElementById('root'));
+ReactDOM.render(<TodoContainer />, document.getElementById('root'));
