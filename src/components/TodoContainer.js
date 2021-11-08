a0bfb99fb1a5e4ffcf6278ccf6270997e061ff01
King Josaphat Chewa <26507383+Kingjosh007@users.noreply.github.com>
2021-11-08 23:41:02 +0100

    Create state in TodoContainer.js

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -2,13 +2,34 @@ import React from 'react';
 
 // eslint-disable-next-line react/prefer-stateless-function
 class TodoContainer extends React.Component {
-  render() {
-    return (
-      <>
-        <h1>Hello from Create React App</h1>
-        <p>I am in a React Component!</p>
-      </>
-    );
-  }
+    state = {
+      todos: [
+        {
+          id: 1,
+          title: 'Setup development environment',
+          completed: true,
+        },
+        {
+          id: 2,
+          title: 'Develop website and add content',
+          completed: false,
+        },
+        {
+          id: 3,
+          title: 'Deploy to live server',
+          completed: false,
+        },
+      ],
+    };
+
+    render() {
+      return (
+        <ul>
+          {this.state.todos.map((todo) => (
+            <li>{todo.title}</li>
+          ))}
+        </ul>
+      );
+    }
 }
 export default TodoContainer;
