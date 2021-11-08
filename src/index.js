cec1484ca7359b730332f5c08b52b50a11ff78e7
King Josaphat Chewa <26507383+Kingjosh007@users.noreply.github.com>
2021-11-08 23:37:44 +0100

    Refactor to use StrictMode

--- src/index.js
+++ src/index.js
@@ -4,4 +4,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import TodoContainer from './components/TodoContainer';
 
-ReactDOM.render(<TodoContainer />, document.getElementById('root'));
+ReactDOM.render(
+  <React.StrictMode>
+    <TodoContainer />
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
