97cac815215f59bc37267c9df42e12e74d54e114
King Josaphat Chewa <26507383+Kingjosh007@users.noreply.github.com>
2021-11-09 00:48:34 +0100

    Refactor TodoItem to a functional component

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,14 +1,9 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
 
-// eslint-disable-next-line react/prefer-stateless-function
-class TodoItem extends React.Component {
-  render() {
-    const { todo } = this.props;
-    return (
-      <li>{todo.title}</li>
-    );
-  }
+function TodoItem(props) {
+  const { todo } = props;
+  return <li>{todo.title}</li>;
 }
 
 export default TodoItem;
