ef4475c5f2a7ab038bd15a98473d898c49552109
King Josaphat Chewa <26507383+Kingjosh007@users.noreply.github.com>
2021-11-09 11:32:49 +0100

    Make input a controlled field using state in InputTodo

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -1,11 +1,17 @@
+/* eslint-disable react/state-in-constructor */
 import React, { Component } from 'react';
 
 // eslint-disable-next-line react/prefer-stateless-function
 class InputTodo extends Component {
+  state = {
+    title: '',
+  };
+
   render() {
+    const { title } = this.state;
     return (
       <form>
-        <input type="text" placeholder="Add Todo..." />
+        <input type="text" placeholder="Add Todo..." value={title} />
         <button type="submit">Submit</button>
       </form>
     );
