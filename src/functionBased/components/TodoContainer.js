d52f762b1116fbe7ebdb67dff76f65f86ce5585f
King Josaphat Chewa <26507383+Kingjosh007@users.noreply.github.com>
2021-12-01 14:32:54 +0100

    Create folder classBased and functionBased for reference

---
+++ src/functionBased/components/TodoContainer.js
@@ -0,0 +1,99 @@
+import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import Header from './Header';
+import InputTodo from './InputTodo';
+import TodosList from './TodosList';
+import { capitalize } from '../utils/stringManipulations';
+import { getFromLocalStorage, saveToLocalStorage } from '../utils/storage';
+
+// eslint-disable-next-line react/prefer-stateless-function
+class TodoContainer extends React.Component {
+    // eslint-disable-next-line react/state-in-constructor
+    state = {
+      todos: [],
+    };
+
+    // Life cycle methods
+    componentDidMount() {
+      const todos = getFromLocalStorage('todos');
+      if (todos) {
+        this.setState({ todos });
+      } else {
+        fetch('https://jsonplaceholder.typicode.com/todos?_limit=10')
+          .then((response) => response.json())
+          .then((data) => {
+            console.log(data);
+            this.setState({
+              todos: data.map((td) => {
+                const newTodo = { ...td };
+                newTodo.title = capitalize(td.title);
+                return newTodo;
+              }),
+            });
+          });
+      }
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+      const { todos } = this.state;
+      if (prevState.todos !== todos) {
+        saveToLocalStorage('todos', todos);
+      }
+    }
+
+    // Methods
+
+    delTodo = (id) => {
+      this.setState((prevState) => ({
+        todos: prevState.todos.filter((todo) => todo.id !== id),
+      }));
+    };
+
+    addTodoItem = (title) => {
+      const { todos } = this.state;
+      const newTodo = {
+        id: uuidv4(),
+        title,
+        completed: false,
+      };
+      if (todos.find((td) => td.title === title)) {
+        alert('A to-do item already has this title. Please change it');
+      } else {
+        this.setState({
+          todos: [...todos, newTodo],
+        });
+      }
+    };
+
+    setUpdate = (updatedTitle, id) => {
+      this.setState((prevState) => ({
+        todos: prevState.todos.map((todo) => {
+          if (todo.id === id) {
+            return {
+              ...todo, title: updatedTitle,
+            };
+          }
+          return todo;
+        }),
+      }));
+    }
+
+    render() {
+      const { todos } = this.state;
+      return (
+        <div className="container">
+          <div className="inner">
+            <Header />
+            <InputTodo addTodoProps={this.addTodoItem} />
+            <TodosList
+              todos={todos}
+              handleChangeProps={this.handleChange}
+              deleteTodoProps={this.delTodo}
+              setUpdateProps={this.setUpdate}
+            />
+          </div>
+        </div>
+      );
+    }
+}
+export default TodoContainer;
